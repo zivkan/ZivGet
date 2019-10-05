36c39583b8832dde4529f57e111129a046ef1ba3
Andy Zivkovic <zivkovic.andy@gmail.com>
2019-10-05 12:47:00 -0700

    Bootstrap solution

---
+++ src/ZivGet/Program.cs
@@ -0,0 +1,14 @@
+﻿// Licensed under the MIT license. See the LICENSE file in the project root for more information.
+
+namespace ZivGet
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/ZivGet.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+// Licensed under the MIT license. See the LICENSE file in the project root for more information.
+
+namespace ZivGet.UnitTests
+{
+    using System;
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
